f13d9243d3fab837a3312f9e8bcd8bbc39ce6925
Shubham Nagpal <shubhamnagpal018@gmail.com>
2025-11-22 00:01:40 +0530

    Render port fix

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Backend running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on port ${PORT}`);
 });
